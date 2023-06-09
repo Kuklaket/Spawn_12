8e0a50f12a8572e2423904d29308e11340b64c42
Kuklaket <smakova.2001.ru@gmail.com>
2023-06-09 11:34:06 +0300

    Create EnemyCat.cs

---
+++ Assets/Scripts/EnemyCat.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCat : MonoBehaviour
+{
+    private int _horizontalMove;
+    private int _verticalMove;
+    private int _speed = 1;
+
+    private void Start()
+    {
+        gameObject.SetActive(true);
+        _horizontalMove = ConvertRandomValueInDirectionMove();
+        _verticalMove = ConvertRandomValueInDirectionMove();
+
+        StartCoroutine(StartLive());
+    }
+
+    void Update()
+    {
+        transform.Translate(_speed * Time.deltaTime * _horizontalMove, _speed * Time.deltaTime * _verticalMove, 0);
+    }
+
+    public int ConvertRandomValueInDirectionMove()
+    {
+        int directionCount;
+        int directMove = 1;
+        int reversMove = -1;
+        int countReversMove = 2;
+
+        if (Random.Range(0, countReversMove) == 0)
+        {
+            directionCount = reversMove;
+        }
+        else 
+        {
+            directionCount = directMove;
+        }
+
+        return directionCount;
+    }
+
+    private IEnumerator StartLive()
+    {
+        int liveLenght = 3;
+              
+        yield return new WaitForSeconds(liveLenght);
+        Destroy(gameObject);
+    }
+}
