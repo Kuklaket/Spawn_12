cc3760ce9393000e9d1fa1993c4d4de0502feb50
Kuklaket <smakova.2001.ru@gmail.com>
2023-06-22 21:26:33 +0300

    Update EnemyCat.cs

--- Assets/Scripts/EnemyCat.cs
+++ Assets/Scripts/EnemyCat.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class EnemyCat : MonoBehaviour
 {
-    private int _horizontalMove;
-    private int _verticalMove;
+    private Vector2 _vectorMove = new Vector2();   
     private int _speed = 1;
 
     private void Start()
     {
         gameObject.SetActive(true);
-        _horizontalMove = ConvertRandomValueInDirectionMove();
-        _verticalMove = ConvertRandomValueInDirectionMove();
+        ConvertValueInDirection();
 
         StartCoroutine(StartLive());
     }
 
     private void Update()
     {
-        transform.Translate(_speed * Time.deltaTime * _horizontalMove, _speed * Time.deltaTime * _verticalMove, 0);
+        transform.Translate(_vectorMove.normalized * _speed * Time.deltaTime);
     }
 
-    public int ConvertRandomValueInDirectionMove()
+    private void ConvertValueInDirection()
+    {
+        _vectorMove.x = GetDirection();
+
+        do
+        {
+            _vectorMove.y = GetDirection();
+
+        } while (_vectorMove.x == 0 && _vectorMove.y == 0);
+    }
+
+    private int GetDirection()
     {
         int directionCount;
         int directMove = 1;
         int reversMove = -1;
         int countReversMove = 2;
+        int randomValue = Random.Range(reversMove, countReversMove);
 
-        if (Random.Range(0, countReversMove) == 0)
+        if (randomValue == reversMove)
         {
             directionCount = reversMove;
         }
-        else 
+        else if (randomValue == 0)
+        {
+            directionCount = 0;
+        }
+        else
         {
             directionCount = directMove;
         }
