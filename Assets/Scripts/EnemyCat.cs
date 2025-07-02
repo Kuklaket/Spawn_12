d9b65eb644380a4a83b7158d28a23b4171c68f8c
Kuklaket <smakova.2001.ru@gmail.com>
2025-07-03 00:12:32 +0300

    0.1

--- Assets/Scripts/EnemyCat.cs
+++ Assets/Scripts/EnemyCat.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyCat : MonoBehaviour
 {
-    private Vector2 _vectorMove = new Vector2();   
+    private Vector2 _vectorMove;   
     private int _speed = 1;
 
-    private void Start()
-    {
-        gameObject.SetActive(true);
-        ConvertValueInDirection();
+    public event Action<EnemyCat> ReadyForRelease;
 
+    private void OnEnable()
+    {
         StartCoroutine(StartLive());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
     private void Update()
@@ -20,46 +24,16 @@ public class EnemyCat : MonoBehaviour
         transform.Translate(_vectorMove.normalized * _speed * Time.deltaTime);
     }
 
-    private void ConvertValueInDirection()
-    {
-        _vectorMove.x = GetDirection();
-
-        do
-        {
-            _vectorMove.y = GetDirection();
-
-        } while (_vectorMove.x == 0 && _vectorMove.y == 0);
-    }
-
-    private int GetDirection()
-    {
-        int directionCount;
-        int directMove = 1;
-        int reversMove = -1;
-        int countReversMove = 2;
-        int randomValue = Random.Range(reversMove, countReversMove);
-
-        if (randomValue == reversMove)
-        {
-            directionCount = reversMove;
-        }
-        else if (randomValue == 0)
-        {
-            directionCount = 0;
-        }
-        else
-        {
-            directionCount = directMove;
-        }
-
-        return directionCount;
-    }
-
     private IEnumerator StartLive()
     {
-        int liveLenght = 3;
-              
+        int liveLenght = 5;
+
         yield return new WaitForSeconds(liveLenght);
-        Destroy(gameObject);
+        ReadyForRelease?.Invoke(this);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        _vectorMove = direction;
     }
 }
--- Assets/Scripts/SpawnPointEnemies.cs
+++ Assets/Scripts/SpawnPointEnemies.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class SpawnPointEnemies : MonoBehaviour
 {
-    [SerializeField] private EnemyCat _enemy;
-    
+    [SerializeField] private EnemyCat _enemyPrefab;
+    [SerializeField] private int _defaultCapacity = 3;
+    [SerializeField] private int _maxPoolSize = 3;
+
     private List<Transform> _pointsSpawn = new();
+    private ObjectPool<EnemyCat> _enemyPool;
+
+    private void Awake()
+    {
+        FillList();
+
+        _enemyPool = new ObjectPool<EnemyCat>(
+            createFunc: CreateEnemy,
+            actionOnGet: (enemy) => {
+                enemy.transform.position = SelectRandomPoint(_pointsSpawn).position;
+                enemy.gameObject.SetActive(true);
+                enemy.SetDirection(GetRandomDirection());
+            },
+            actionOnRelease: (enemy) => enemy.gameObject.SetActive(false),
+            actionOnDestroy: (enemy) => Destroy(enemy.gameObject),
+            collectionCheck: true,
+            defaultCapacity: _defaultCapacity,
+            maxSize: _maxPoolSize
+        );
+    }
 
     private void Start()
     {
@@ -17,23 +38,30 @@ public class SpawnPointEnemies : MonoBehaviour
 
     private IEnumerator SpawnEnemy()
     {
-        int waitingInSeconds = 2;
+        float waitingInSeconds = 2f;
         var waitForSeconds = new WaitForSeconds(waitingInSeconds);
-
-        FillList();
             
         while (true)
         {
-            Initialized(_enemy);
+            _enemyPool.Get();
             yield return waitForSeconds;
         }      
     }
 
-    private void Initialized(EnemyCat newEnemy)
+    private EnemyCat CreateEnemy()
     {
         Transform point = SelectRandomPoint(_pointsSpawn);
+        EnemyCat enemy = Instantiate(_enemyPrefab, point.position, Quaternion.identity);
 
-        newEnemy = Instantiate(newEnemy, new Vector2(point.transform.position.x, point.transform.position.y), Quaternion.identity);
+        if (enemy.TryGetComponent<EnemyCat>(out EnemyCat _cubeComponent))
+            enemy.ReadyForRelease += ReleaseEnemyInPool;
+
+        return enemy;
+    }
+
+    private void ReleaseEnemyInPool(EnemyCat enemy)
+    {
+        _enemyPool.Release(enemy);
     }
 
     private Transform SelectRandomPoint(List<Transform> spawnList)
@@ -41,7 +69,18 @@ public class SpawnPointEnemies : MonoBehaviour
         int _randomPointNumber = Random.Range(0, spawnList.Count);
 
         Transform point = spawnList[_randomPointNumber];
+
         return point;
+    }
+
+    private Vector2 GetRandomDirection()
+    {
+        float fullCircle = 2f * Mathf.PI;
+        float randomAngle = Random.Range(0f, fullCircle);
+
+        Vector2 directionAngle = new(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle));
+
+        return directionAngle;
     }
 
     private void FillList()
